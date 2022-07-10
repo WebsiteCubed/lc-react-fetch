ac67132555ba6d8a0a0ec2b43c0a172f8a75467e
WebsiteCubed <=>
2022-07-10 15:15:34 +0100

    commit

---
+++ src/Joke.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useQuery } from 'react-query'
+import useFetch from './useFetch'
+
+export default function Joke() {
+  // const { data: joke, isLoading, errorMessage } = useFetch('https://official-joke-api.appspot.com/jokes/random')
+  
+  const { data: joke, isLoading, isError, error, isSuccess 
+    } = useQuery('joke', fetchJoke, {staleTime: 6000, refetchOnWindowFocus: false})
+
+  function fetchJoke() {
+    return fetch('https://official-joke-api.appspot.com/jokes/random')
+      .then(response => response.json())
+  }
+
+  return (
+    <div>
+      <h2>Joke API</h2>
+      {isLoading && <div>Loading...</div>}
+      {isSuccess && (
+        <div>{joke.setup + ' ' + joke.punchline}</div>
+      )}
+      {isError && <div>{error.message}</div>}
+    </div>
+  )
+}
---
+++ src/Reddit.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useQuery } from 'react-query'
+import useFetch from './useFetch'
+
+export default function Reddit() {
+  // const { data: posts, isLoading, errorMessage } = useFetch('https://www.reddit.com/r/space.json')
+  
+  const { data: posts, isLoading, isError, error, isSuccess 
+    } = useQuery('posts', fetchPosts, {retry: false,})
+
+  function fetchPosts() {
+    return fetch('https://www.reddit.com/r/space.json')
+      .then(response => response.json())
+  }
+
+  return (
+    <div>
+      <h2>Reddit API</h2>
+      {isLoading && <div>Loading...</div>}
+      { isSuccess && (
+        <ul>
+          {posts.data.children.map(post => (
+          <li key={post.data.id}>
+              <img src={`${post.data.thumbnail}`} alt="" />
+            <a href={`https://reddit.com${post.data.permalink}`}>{post.data.title}</a>
+            </li>
+          ))}    
+        </ul>
+      )}
+      {isError && <div>{ error.message }</div>}
+    </div>
+  )
+}
